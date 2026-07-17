74272ed5e7c82c4ec23ba871cd8441ba63c8f23d
DDRNFinch <dfinch1984@hotmail.co.uk>
2026-07-18 00:13:52 +0300

    Add files via upload

--- course-bridge.js
+++ course-bridge.js
@@ -9,4 +9,22 @@
   if(settings.course&&settings.course!==course){
     location.replace("../../");
   }
+
+  if(course==="brick"){
+    if(!document.querySelector('link[data-brick-professional-navigation]')){
+      const link=document.createElement("link");
+      link.rel="stylesheet";
+      link.href="./professional-navigation.css?v=direct-1";
+      link.dataset.brickProfessionalNavigation="true";
+      document.head.appendChild(link);
+    }
+
+    if(!document.querySelector('script[data-brick-professional-navigation]')){
+      const script=document.createElement("script");
+      script.src="./professional-navigation.js?v=direct-1";
+      script.defer=true;
+      script.dataset.brickProfessionalNavigation="true";
+      document.head.appendChild(script);
+    }
+  }
 })();
